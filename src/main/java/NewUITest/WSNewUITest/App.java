9dd5b93147e65f2f30a23f2b14321ba26a1bc65a
dexterchan <dexterchan@gmail.com>
2017-04-08 14:58:07 +0800

    enable jar

--- src/main/java/NewUITest/WSNewUITest/App.java
+++ src/main/java/NewUITest/WSNewUITest/App.java
@@ -1,6 +1,7 @@
 package NewUITest.WSNewUITest;
 
 import java.io.IOException;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,57 +15,51 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import NewUITest.WSNewUITest.Service.MyEchoServlet;
 
-public class App extends AbstractHandler
-{
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response)
-        throws IOException, ServletException
-    {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
-    }
+public class App extends AbstractHandler {
+	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+		baseRequest.setHandled(true);
+		response.getWriter().println("<h1>Hello World</h1>");
+	}
 
-    public static void main(String[] args) throws Exception
-    {
-        
-        //server.setHandler(new HelloWorld());
-        
-        String webappDirLocation = "src/main/resources/static/";
+	public static void main(String[] args) throws Exception {
 
-        
-        Server server = new Server(8080);
-        
-        
+		// server.setHandler(new HelloWorld());
 
-        ServletContextHandler context = new ServletContextHandler(
-                ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        context.setResourceBase(webappDirLocation);
-        server.setHandler(context);
+		Server server = new Server(8080);
 
-        // Add dump servlet
-        //context.addServlet(DumpServlet.class, "/dump/*");
-        // Add default servlet
-        context.addServlet(DefaultServlet.class, "/");
+		// Figure out what path to serve content from
+		ClassLoader cl = App.class.getClassLoader();
+		URL f = cl.getResource("static");
+		if (f == null) {
+			throw new RuntimeException("Unable to find resource directory");
+		}
+		System.out.println("Static file is " + f.toExternalForm());
+		
 
-        Server wsserver = new Server(9001);
-        ServletContextHandler wscontext = new ServletContextHandler(
-                ServletContextHandler.SESSIONS);
-        wscontext.addServlet(MyEchoServlet.class, "/");
-        wsserver.setHandler(wscontext);
-        
-        wsserver.start();
-        
-        server.start();
-        
-        
-        server.join();
-        wsserver.join();
-        
+		String webappDirLocation = "src/main/resources/static/";
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		context.setResourceBase(f.toExternalForm());
+		server.setHandler(context);
 
-    }
+		
+		// Add default servlet
+		context.addServlet(DefaultServlet.class, "/");
+
+		Server wsserver = new Server(9001);
+		ServletContextHandler wscontext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		wscontext.addServlet(MyEchoServlet.class, "/");
+		wsserver.setHandler(wscontext);
+
+		wsserver.start();
+
+		server.start();
+
+		server.join();
+		wsserver.join();
+
+	}
 }
