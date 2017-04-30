5cfce897e7f26321a36e92154719f3d27973d54e
dexterchan <dexterchan@gmail.com>
2017-04-30 15:38:50 +0800

    unique voice file per session

--- src/main/java/NewUITest/WSNewUITest/Service/MyEchoSocket.java
+++ src/main/java/NewUITest/WSNewUITest/Service/MyEchoSocket.java
@@ -15,12 +15,32 @@ import org.eclipse.jetty.websocket.api.RemoteEndpoint;
 import org.eclipse.jetty.websocket.api.Session;
 
 import com.google.speech.GoogleSpeechReco;
-
+import java.util.UUID;
 public class MyEchoSocket implements org.eclipse.jetty.websocket.api.WebSocketListener {
 	private Session outbound;
 	
 	String currentCommand="";
+	
+	String voiceFolder = "voiceData";
+	String voiceFile = "newout.raw";
+	
+	public MyEchoSocket(){
+		//Create a folder if not found
+		checkandCreateFolder();
+	}
 
+	private void checkandCreateFolder(){
+		try{
+			File directory = new File(voiceFolder);
+		    if (! directory.exists()){
+		        directory.mkdir();
+		        // If you require it to make the entire directory path including parents,
+		        // use directory.mkdirs(); here instead.
+		    }
+			
+		}catch (Exception e){e.printStackTrace();}
+	}
+	
 	@Override
 	public void onWebSocketBinary(byte[] data, int offset, int len) {
 		/* only interested in text messages */
@@ -31,19 +51,10 @@ public class MyEchoSocket implements org.eclipse.jetty.websocket.api.WebSocketLi
 		try {
 
 			if (currentCommand.equals("start")) {
-				/*try {
-					// The temporary file that contains our captured audio stream
-					File f = new File("out.wav");
-					FileOutputStream fOut = new FileOutputStream("out.wav",true);
-					fOut.write(data);
-					fOut.close();
-					// if the file already exists we append it.
-						
-				} catch (Exception e) {	e.printStackTrace();}*/
 				
 				try {
 					// The temporary file that contains our captured audio stream
-					File f = new File("out.wav");
+					File f = new File(this.voiceFile);
  
 					// if the file already exists we append it.
 					if (f.exists()) {
@@ -63,16 +74,16 @@ public class MyEchoSocket implements org.eclipse.jetty.websocket.api.WebSocketLi
 						 // write out the output to a temporary file
 		                    AudioSystem.write(appendedFiles, 
 		                            AudioFileFormat.Type.WAVE,
-		                            new File("out2.wav"));
+		                            new File(this.voiceFile+"2"));
  
 		                    // rename the files and delete the old one
-		                    File f1 = new File("out.wav");
-		                    File f2 = new File("out2.wav");
+		                    File f1 = new File(this.voiceFile);
+		                    File f2 = new File(this.voiceFile+"2");
 		                    f1.delete();
-		                    f2.renameTo(new File("out.wav"));
+		                    f2.renameTo(new File(this.voiceFile));
 					} else {
 						System.out.println("Starting new recording.");
-						FileOutputStream fOut = new FileOutputStream("out.wav",true);
+						FileOutputStream fOut = new FileOutputStream(this.voiceFile,true);
 						fOut.write(data);
 						fOut.close();
 					}			
@@ -107,6 +118,8 @@ public class MyEchoSocket implements org.eclipse.jetty.websocket.api.WebSocketLi
 			// before we start we cleanup anything left over
 			cleanup();
 			currentCommand = "start";
+			UUID uniqueKey = UUID.randomUUID();
+			this.voiceFile = this.voiceFolder+File.separator+uniqueKey.toString()+".raw";
 		} else if (data.startsWith("stop")) {
 			currentCommand = "stop";
 		} else if (data.startsWith("clear")) {
@@ -118,7 +131,7 @@ public class MyEchoSocket implements org.eclipse.jetty.websocket.api.WebSocketLi
 			
 			try
 			{
-				List<GoogleSpeechReco.RecoResult> r=GoogleSpeechReco.syncRecognizeFile("out.wav");
+				List<GoogleSpeechReco.RecoResult> r=GoogleSpeechReco.syncRecognizeFile(voiceFile);
 				
 				if(r.size()>0){
 					GoogleSpeechReco.RecoResult result = r.get(0);
@@ -139,7 +152,7 @@ public class MyEchoSocket implements org.eclipse.jetty.websocket.api.WebSocketLi
 	public void cleanup(){
 		try{
 
-		File f = new File("out.wav");
+		File f = new File(voiceFile);
 		f.delete();
 		}catch(Exception e){}
 		
